c7c9beafe566b26f4fbcb02754efc63a3fbc0d59
AlexKiryushkin <sashakir94@mail.ru>
2025-10-30 12:37:10 +0700

    #57260339 Implemented usage of ICameraListener in ViewController
    - added callback calls on rotate, zoom, rotate, start and stop of camera animation

--- src/OccSceneInteractionService/include/OccSceneInteractionService/ViewController.h
+++ src/OccSceneInteractionService/include/OccSceneInteractionService/ViewController.h
@@ -3,16 +3,64 @@
 #define OCC_SCENE_INTERACTION_SERVICE_VIEW_CONTROLLER_H
 
 #include <OccSceneInteractionService/OccSceneInteractionServiceExports.h>
+#include <OccSceneInteractionService/UiRenderSyncObject.h>
 
 #include <AIS_ViewController.hxx>
+#include <Standard_Handle.hxx>
 
 namespace osis
 {
+
+class ICameraListener;
 
 class ViewController : public AIS_ViewController
 {
   public:
     OSIS_API ViewController();
+
+    /**
+     * @brief Sets camera listener. Can be NULL, if no camera listener is needed. Is called from UI thread.
+     * @param pCameraListener camera listener.
+     */
+    void setCameraListener(Handle(ICameraListener) pCameraListener);
+
+  public: //! @name public overridden methods
+    /**
+     * @brief Overridden method of HandleViewEvents. Is called from Render thread.
+     */
+    void HandleViewEvents(const Handle(AIS_InteractiveContext) &pContext,
+                          const Handle(V3d_View) &pView) override;
+
+  protected: //! @name protected overridden methods
+    /**
+     * @brief Overridden method of flushBuffers. Is called at sync stage between UI and Render threads.
+     */
+    void flushBuffers(const Handle(AIS_InteractiveContext) &pContext,
+                      const Handle(V3d_View) &pView) override;
+
+    /**
+     * @brief Overridden method of handlePanning. Is called from Render thread.
+     */
+    void handlePanning(const Handle(V3d_View) &view) override;
+
+    /**
+     * @brief Overridden method of handleZoom. Is called from Render thread.
+     */
+    void handleZoom(const Handle(V3d_View) &view, const Aspect_ScrollDelta &params, const gp_Pnt *point) override;
+
+    /**
+     * @brief Overridden method of handleOrbitRotation. Is called from Render thread.
+     */
+    void handleOrbitRotation(const Handle(V3d_View) &view, const gp_Pnt &point, bool toLockZUp) override;
+
+    /**
+     * @brief Overridden method of handleViewRotation. Is called from Render thread.
+     */
+    void handleViewRotation(const Handle(V3d_View) &view, double yawExtra, double pitchExtra, double roll,
+                            bool toRestartOnIncrement) override;
+
+  private:
+    UiRenderSyncObject<Handle(ICameraListener)> m_pCameraListenerSyncObject;
 };
 
 } // namespace osis
--- src/OccSceneInteractionService/src/ViewController.cpp
+++ src/OccSceneInteractionService/src/ViewController.cpp
@@ -1,12 +1,94 @@
 
 #include "OccSceneInteractionService/ViewController.h"
 
+#include "OccSceneInteractionService/ICameraListener.h"
+
+#include <AIS_AnimationCamera.hxx>
+#include <V3d_View.hxx>
+
 namespace osis
 {
 
 ViewController::ViewController()
     : AIS_ViewController{}
 {
+}
+
+void ViewController::setCameraListener(Handle(ICameraListener) pCameraListener)
+{
+    m_pCameraListenerSyncObject.setUiData(std::move(pCameraListener));
+}
+
+void ViewController::HandleViewEvents(const Handle(AIS_InteractiveContext) & pContext, const Handle(V3d_View) & pView)
+{
+    const auto wasAnimationStopped = myViewAnimation && myViewAnimation->IsStopped();
+    AIS_ViewController::HandleViewEvents(pContext, pView);
+
+    if (myViewAnimation)
+    {
+        const auto isAnimationStopped = myViewAnimation->IsStopped();
+
+        auto pCameraListener = m_pCameraListenerSyncObject.getRenderData();
+        if(!wasAnimationStopped && isAnimationStopped)
+        {
+            pCameraListener->onAnimationStopped();
+        }
+
+        if(wasAnimationStopped && !isAnimationStopped)
+        {
+            pCameraListener->onAnimationStarted();
+        }
+    }
+}
+
+void ViewController::flushBuffers(const Handle(AIS_InteractiveContext) &pContext, const Handle(V3d_View) &pView)
+{
+    m_pCameraListenerSyncObject.sync();
+}
+
+void ViewController::handlePanning(const Handle(V3d_View) &view)
+{
+    AIS_ViewController::handlePanning(view);
+
+    auto pCameraListener = m_pCameraListenerSyncObject.getRenderData();
+    if(myToAllowPanning && myGL.Panning.ToPan && pCameraListener)
+    {
+        pCameraListener->onCameraCenterChanged(view->Camera()->Center());
+    }
+}
+
+void ViewController::handleZoom(const Handle(V3d_View) &view, const Aspect_ScrollDelta &params, const gp_Pnt *point)
+{
+    AIS_ViewController::handleZoom(view, params, point);
+
+    auto pCameraListener = m_pCameraListenerSyncObject.getRenderData();
+    if(myToAllowZooming && pCameraListener)
+    {
+        pCameraListener->onCameraScaleChanged(params.Delta);
+    }
+}
+
+void ViewController::handleOrbitRotation(const Handle(V3d_View) &view, const gp_Pnt &point, bool toLockZUp) {
+
+    AIS_ViewController::handleOrbitRotation(view, point, toLockZUp);
+
+    auto pCameraListener = m_pCameraListenerSyncObject.getRenderData();
+    if(myToAllowRotation && myGL.OrbitRotation.ToRotate && pCameraListener)
+    {
+        pCameraListener->onCameraRotation();
+    }
+}
+
+void ViewController::handleViewRotation(const Handle(V3d_View) &view, double yawExtra, double pitchExtra, double roll,
+                                        bool toRestartOnIncrement)
+{
+    AIS_ViewController::handleViewRotation(view, yawExtra, pitchExtra, roll, toRestartOnIncrement);
+
+    auto pCameraListener = m_pCameraListenerSyncObject.getRenderData();
+    if(myToAllowRotation && myGL.ViewRotation.ToRotate && pCameraListener)
+    {
+        pCameraListener->onCameraRotation();
+    }
 }
 
 } // namespace osis
